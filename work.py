dd1494c8fa134880a065179976b7527394cc21db
AnuKBasheer <anuofficial444@gmail.com>
2025-12-10 13:37:39 +0530

    Update work.py

--- work.py
+++ work.py
@@ -1,2 +1,16 @@
 def add(x,y):
     return x+y
+def add(x,y):
+    return x+y
+def add(x,y):
+    return x+y
+def add(x,y):
+    return x+y
+def add(x,y):
+    return x+y
+def add(x,y):
+    return x+y
+def add(x,y):
+    return x+y
+def add(x,y):
+    return x+y
